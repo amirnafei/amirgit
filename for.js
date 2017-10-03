a61cf9009c18ced9eaae865c774b4bb17bf9a7fa
Amir Nafei <amirnafei@MBAe15C02Q4D0CG940.local>
2017-10-03 13:54:32 -0400

    Amir's Repo

---
+++ for.js
@@ -0,0 +1,5 @@
+function ave(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+
+console.log(ave(2,3,1));
---
+++ if-else.js
@@ -0,0 +1,8 @@
+var isRaining = true;
+var temp = 20;
+if (isRaining && temp < 15) {
+    console.log("wear a jacket!");
+}
+else {
+    console.log("no jacket");
+}
